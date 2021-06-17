1f087f838fbbc77845ec6b9604385e2492ef60ce
QwQe308 <78683793+QwQe308@users.noreply.github.com>
2021-06-17 23:18:57 +0800

    update

--- chs.js
+++ chs.js
@@ -197,6 +197,7 @@ var cnItems = {
     'Emit Blue Waves': '发射蓝光波',
     'Challenges are now checked for completion before autobuyer actions.': '现在，在自动购买器起效之前，将检查挑战是否完成。',
     'Dimensions are working as if there are x^0.5of them': '维度数量以 x^0.5 的效率起效<p>汉化优化:QwQ(qq3174905334)</p>',
+    'of any resource':' 资源(对所有资源)'
     'dimensions of each kind': ' 级维度',
     'You need to break Infinity in order to complete Vacuumic challenges.': '你需要打破无限才能完成真空挑战。',
     'You need to get a certain amount of matter to complete Photonic challenges.': '你需要获得一定数量的物质来完成光子挑战。',
